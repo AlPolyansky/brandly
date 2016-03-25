bf55be54c8446ec068eac3587cbc77fb90e51c61
Eclerr <eclerrrr@yandex.ru>
2016-03-25 06:05:30 +0300

    fix header, add one section, add @media

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	$('header').height($(window).height());
+	$('.header_wrap').height($(window).height());
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})
+	$("#toggle").click(function() {
+	  $(this).toggleClass("on");
+	});
 })
